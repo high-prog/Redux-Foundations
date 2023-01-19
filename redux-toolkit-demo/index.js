1a6aabdc2fb169b9755f18516b3a8166a6930706
high-prog <58605707+high-prog@users.noreply.github.com>
2023-01-19 22:28:59 +0530

    5 files added

---
+++ redux-toolkit-demo/index.js
@@ -0,0 +1,5 @@
+//Creating a cake and icecream store like in normal redux was done but here with toolkit 
+//The folder setup is a convention and can be used
+//The app folder contains the store and the feature will be stored in its own folder but in different files for  cake and icecream
+
+ 
